778a0e5fc89ce6ef39560edf3f24247bfe20d0aa
rflteeuwen <rflteeuwen@gmail.com>
2016-12-23 13:41:17 +0100

    Load layers (start button)

--- EvacuationAlarm/evacuation_alarm_dockwidget.py
+++ EvacuationAlarm/evacuation_alarm_dockwidget.py
@@ -25,6 +25,12 @@ import os
 
 from PyQt4 import QtGui, uic
 from PyQt4.QtCore import pyqtSignal
+from qgis.core import *
+from qgis.gui import *
+from PyQt4.QtCore import *
+from PyQt4 import QtGui
+import os, sys
+import qgis
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'evacuation_alarm_dockwidget_base.ui'))
@@ -51,12 +57,161 @@ class EvacuationAlarmDockWidget(QtGui.QDockWidget, FORM_CLASS):
         # test
         self.chemicals_yes.clicked.connect(self.print_yes)
         self.chemicals_no.clicked.connect(self.print_no)
+        self.start.clicked.connect(self.loadLayers)
 
     def print_yes(self):
         print 'chemicals'
 
     def print_no(self):
         print 'no chemicals'
+
+    def loadLayers(self):
+
+
+        # create Qt widget
+        canvas = QgsMapCanvas()
+        canvas.setCanvasColor(Qt.white)
+
+        # enable this for smooth rendering
+        canvas.enableAntiAliasing(True)
+
+        # not updated US6SP10M files from ENC_ROOT
+        source_dir = "C:\sample_data"
+
+        canvas_layers = []
+
+        # load vector layers
+        for files in os.listdir(source_dir):
+
+            # load only the shapefiles
+            if files.endswith(".shp"):
+                vlayer = QgsVectorLayer(source_dir + "/" + files, files, "ogr")
+
+                # add layer to the registry
+                QgsMapLayerRegistry.instance().addMapLayer(vlayer)
+
+                # set extent to the extent of our layer
+
+                canvas_layers.append(QgsMapCanvasLayer(vlayer))
+
+        # refresh canvas and show it
+
+        canvas.setLayerSet(canvas_layers)
+        canvas.refresh()
+        canvas.show()
+
+
+
+
+    #######
+    #   Data functions
+    #######
+    def openScenario(self, filename=""):
+        scenario_open = False
+        scenario_file = os.path.join(u'/Users/jorge/github/GEO1005', 'sample_data', 'time_test.qgs')
+        # check if file exists
+        if os.path.isfile(scenario_file):
+            self.iface.addProject(scenario_file)
+            scenario_open = True
+        else:
+            last_dir = uf.getLastDir("SDSS")
+            new_file = QtGui.QFileDialog.getOpenFileName(self, "", last_dir, "(*.qgs)")
+            if new_file:
+                self.iface.addProject(unicode(new_file))
+                scenario_open = True
+        if scenario_open:
+            self.updateLayers()
+
+    def saveScenario(self):
+        self.iface.actionSaveProject()
+
+    def updateLayers(self):
+        layers = uf.getLegendLayers(self.iface, 'all', 'all')
+        self.selectLayerCombo.clear()
+        if layers:
+            layer_names = uf.getLayersListNames(layers)
+            self.selectLayerCombo.addItems(layer_names)
+            self.setSelectedLayer()
+        else:
+            self.selectAttributeCombo.clear()
+            self.clearChart()
+
+    def setSelectedLayer(self):
+        layer_name = self.selectLayerCombo.currentText()
+        layer = uf.getLegendLayerByName(self.iface, layer_name)
+        self.updateAttributes(layer)
+
+    def getSelectedLayer(self):
+        layer_name = self.selectLayerCombo.currentText()
+        layer = uf.getLegendLayerByName(self.iface, layer_name)
+        return layer
+
+    def updateAttributes(self, layer):
+        self.selectAttributeCombo.clear()
+        if layer:
+            self.clearReport()
+            self.clearChart()
+            fields = uf.getFieldNames(layer)
+            if fields:
+                self.selectAttributeCombo.addItems(fields)
+                self.setSelectedAttribute()
+                # send list to the report list window
+                self.updateReport(fields)
+
+    def setSelectedAttribute(self):
+        field_name = self.selectAttributeCombo.currentText()
+        self.updateAttribute.emit(field_name)
+
+    def getSelectedAttribute(self):
+        field_name = self.selectAttributeCombo.currentText()
+        return field_name
+
+    def startCounter(self):
+        # prepare the thread of the timed even or long loop
+        self.timerThread = TimedEvent(self.iface.mainWindow(), self, 'default')
+        self.timerThread.timerFinished.connect(self.concludeCounter)
+        self.timerThread.timerProgress.connect(self.updateCounter)
+        self.timerThread.timerError.connect(self.cancelCounter)
+        self.timerThread.start()
+        # from here the timer is running in the background on a separate thread. user can continue working on QGIS.
+        self.counterProgressBar.setValue(0)
+        self.startCounterButton.setDisabled(True)
+        self.cancelCounterButton.setDisabled(False)
+
+    def cancelCounter(self):
+        # triggered if the user clicks the cancel button
+        self.timerThread.stop()
+        self.counterProgressBar.setValue(0)
+        self.counterProgressBar.setRange(0, 100)
+        try:
+            self.timerThread.timerFinished.disconnect(self.concludeCounter)
+            self.timerThread.timerProgress.disconnect(self.updateCounter)
+            self.timerThread.timerError.disconnect(self.cancelCounter)
+        except:
+            pass
+        self.timerThread = None
+        self.startCounterButton.setDisabled(False)
+        self.cancelCounterButton.setDisabled(True)
+
+    def updateCounter(self, value):
+        self.counterProgressBar.setValue(value)
+
+    def concludeCounter(self, result):
+        # clean up timer thread stuff
+        self.timerThread.stop()
+        self.counterProgressBar.setValue(100)
+        try:
+            self.timerThread.timerFinished.disconnect(self.concludeCounter)
+            self.timerThread.timerProgress.disconnect(self.updateCounter)
+            self.timerThread.timerError.disconnect(self.cancelCounter)
+        except:
+            pass
+        self.timerThread = None
+        self.startCounterButton.setDisabled(False)
+        self.cancelCounterButton.setDisabled(True)
+        # do something with the results
+        self.iface.messageBar().pushMessage("Infor", "The counter results: %s" % result, level=0, duration=5)
+
 
     def closeEvent(self, event):
         self.closingPlugin.emit()
