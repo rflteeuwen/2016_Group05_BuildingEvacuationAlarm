d6b2113d7c2eaf8c16cc2c21dbcfbb7bb32e671b
rflteeuwen <rflteeuwen@gmail.com>
2016-12-28 14:41:18 +0100

    Data folder organized

--- EvacuationAlarm/evacuation_alarm_dockwidget.py
+++ EvacuationAlarm/evacuation_alarm_dockwidget.py
@@ -90,7 +90,6 @@ class EvacuationAlarmDockWidget(QtGui.QDockWidget, FORM_CLASS):
                 # add layer to the registry
                 QgsMapLayerRegistry.instance().addMapLayer(vlayer)
 
-                # set extent to the extent of our layer
 
                 canvas_layers.append(QgsMapCanvasLayer(vlayer))
 
@@ -103,114 +102,7 @@ class EvacuationAlarmDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
 
 
-    #######
-    #   Data functions
-    #######
-    def openScenario(self, filename=""):
-        scenario_open = False
-        scenario_file = os.path.join(u'/Users/jorge/github/GEO1005', 'sample_data', 'time_test.qgs')
-        # check if file exists
-        if os.path.isfile(scenario_file):
-            self.iface.addProject(scenario_file)
-            scenario_open = True
-        else:
-            last_dir = uf.getLastDir("SDSS")
-            new_file = QtGui.QFileDialog.getOpenFileName(self, "", last_dir, "(*.qgs)")
-            if new_file:
-                self.iface.addProject(unicode(new_file))
-                scenario_open = True
-        if scenario_open:
-            self.updateLayers()
 
-    def saveScenario(self):
-        self.iface.actionSaveProject()
-
-    def updateLayers(self):
-        layers = uf.getLegendLayers(self.iface, 'all', 'all')
-        self.selectLayerCombo.clear()
-        if layers:
-            layer_names = uf.getLayersListNames(layers)
-            self.selectLayerCombo.addItems(layer_names)
-            self.setSelectedLayer()
-        else:
-            self.selectAttributeCombo.clear()
-            self.clearChart()
-
-    def setSelectedLayer(self):
-        layer_name = self.selectLayerCombo.currentText()
-        layer = uf.getLegendLayerByName(self.iface, layer_name)
-        self.updateAttributes(layer)
-
-    def getSelectedLayer(self):
-        layer_name = self.selectLayerCombo.currentText()
-        layer = uf.getLegendLayerByName(self.iface, layer_name)
-        return layer
-
-    def updateAttributes(self, layer):
-        self.selectAttributeCombo.clear()
-        if layer:
-            self.clearReport()
-            self.clearChart()
-            fields = uf.getFieldNames(layer)
-            if fields:
-                self.selectAttributeCombo.addItems(fields)
-                self.setSelectedAttribute()
-                # send list to the report list window
-                self.updateReport(fields)
-
-    def setSelectedAttribute(self):
-        field_name = self.selectAttributeCombo.currentText()
-        self.updateAttribute.emit(field_name)
-
-    def getSelectedAttribute(self):
-        field_name = self.selectAttributeCombo.currentText()
-        return field_name
-
-    def startCounter(self):
-        # prepare the thread of the timed even or long loop
-        self.timerThread = TimedEvent(self.iface.mainWindow(), self, 'default')
-        self.timerThread.timerFinished.connect(self.concludeCounter)
-        self.timerThread.timerProgress.connect(self.updateCounter)
-        self.timerThread.timerError.connect(self.cancelCounter)
-        self.timerThread.start()
-        # from here the timer is running in the background on a separate thread. user can continue working on QGIS.
-        self.counterProgressBar.setValue(0)
-        self.startCounterButton.setDisabled(True)
-        self.cancelCounterButton.setDisabled(False)
-
-    def cancelCounter(self):
-        # triggered if the user clicks the cancel button
-        self.timerThread.stop()
-        self.counterProgressBar.setValue(0)
-        self.counterProgressBar.setRange(0, 100)
-        try:
-            self.timerThread.timerFinished.disconnect(self.concludeCounter)
-            self.timerThread.timerProgress.disconnect(self.updateCounter)
-            self.timerThread.timerError.disconnect(self.cancelCounter)
-        except:
-            pass
-        self.timerThread = None
-        self.startCounterButton.setDisabled(False)
-        self.cancelCounterButton.setDisabled(True)
-
-    def updateCounter(self, value):
-        self.counterProgressBar.setValue(value)
-
-    def concludeCounter(self, result):
-        # clean up timer thread stuff
-        self.timerThread.stop()
-        self.counterProgressBar.setValue(100)
-        try:
-            self.timerThread.timerFinished.disconnect(self.concludeCounter)
-            self.timerThread.timerProgress.disconnect(self.updateCounter)
-            self.timerThread.timerError.disconnect(self.cancelCounter)
-        except:
-            pass
-        self.timerThread = None
-        self.startCounterButton.setDisabled(False)
-        self.cancelCounterButton.setDisabled(True)
-        # do something with the results
-        self.iface.messageBar().pushMessage("Infor", "The counter results: %s" % result, level=0, duration=5)
 
 
     def closeEvent(self, event):
